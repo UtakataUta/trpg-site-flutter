2f6496f5866a35fb898319ed54823856f1585df4
UtakataUta <utakata.trpg@gmail.com>
2022-12-22 19:26:44 +0900

    add: about page sentence(#2)

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,125 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: '泡沫のTRPG倉庫',
+      home: AboutPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // device size
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double deviceHeight = MediaQuery.of(context).size.height;
+
+    // text theme
+    final ThemeData theme = Theme.of(context);
+    final TextTheme textTheme = theme.textTheme;
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: SizedBox(
+          width: deviceWidth * 0.95,
+          height: deviceHeight * 0.95,
+          child: Card(
+            child: Text.rich(
+              TextSpan(
+                text: 'ご挨拶\n',
+                style: textTheme.titleLarge,
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'こんにちは。泡沫と申します。このサイトは私のTRPGに関する情報をまとめたものです。\n',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: '経験値\n',
+                    style: textTheme.titleLarge,
+                  ),
+                  TextSpan(
+                    text: '始めたばかりでまだまだひよっこですが、楽しく遊んでいます。PL中心ですが、KP・GMも挑戦中です。',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: 'ココフォリアとDiscordを使ったボイセがメインですが、オフセも経験あります。\n',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: '所持ルルブ\n',
+                    style: textTheme.titleLarge,
+                  ),
+                  TextSpan(
+                    text: '・CoC6版\n',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: '・インセイン (基本/デッドループ/SCP)\n',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: '創作\n',
+                    style: textTheme.titleLarge,
+                  ),
+                  TextSpan(
+                    text: 'イラストを描くのが好きなので、立ち絵や卓中の出来事をイラストとして描いたりします。',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: 'Live2Dも好きなので、たまに立ち絵を動かしたり、動画を作ったりしています。\n',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: 'また、ちょっとだけプログラミングができるので、このWebサイトも頑張って作りました。',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: 'バグやエラーがあれば、お気軽にお知らせいただけると嬉しいです。\n',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: '使用ツール・言語\n',
+                    style: textTheme.titleLarge,
+                  ),
+                  TextSpan(
+                    text: '・Clip Studio Paint\n',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: '・Ibis Paint\n',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: '・Live 2D\n',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: '・Flutter(Dart)\n',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: '・C++\n',
+                    style: textTheme.bodyText1,
+                  ),
+                  TextSpan(
+                    text: '最後に\n',
+                    style: textTheme.titleLarge,
+                  ),
+                  TextSpan(
+                    text: 'セッションのお誘いやTRPGのご依頼などがありましたら、TwitterのDMでご連絡ください。\n',
+                    style: textTheme.bodyText1,
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
